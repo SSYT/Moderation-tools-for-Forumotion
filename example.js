8db904d6ca249f6bc8d511bacebfa7f95b5ed7a7
zeusmaximus <zeusmaximus@users.noreply.github.com>
2017-05-28 01:17:34 +0300

    Create example.js

---
+++ example.js
@@ -0,0 +1,79 @@
+var zModConfig = [{
+    icon: "https://cdn2.iconfinder.com/data/icons/circle-icons-1/64/chat-16.png",
+    source : "https://rawgit.com/zeusmaximus/Moderation-tools-for-Forumotion/b0a3c9c5/script.js",
+    loadCss: true,
+    fontAwesome: true
+}];
+
+var zModTabels = [{
+        type: "zalert",
+        body_start: '[table class="zmod_box zalert"][tr][td style="padding-right: 10px;" width="60px;"][icon="fa fa-exclamation-circle"][/icon][div]',
+        body_end: "[/div][/td][/tr][/table]\n\n"
+    },
+    {
+        type: "zsuccess",
+        body_start: '[table class="zmod_box zsuccess"][tr][td style="padding-right: 10px;" width="60px;"][icon="fa fa-check"][/icon][div]',
+        body_end: "[/div][/td][/tr][/table]\n\n"
+    },
+    {
+        type: "zdefault",
+        body_start: '[table class="zmod_box zdefault"][tr][td style="padding-right: 10px;" width="60px;"][icon="fa fa-rocket"][/icon][div]',
+        body_end: "[/div][/td][/tr][/table]\n\n"
+    },
+    {
+        type: "zwarning",
+        body_start: '[table class="zmod_box zwarning"][tr][td style="padding-right: 10px;" width="60px;"][icon="fa fa-exclamation-triangle"][/icon][div]',
+        body_end: "[/div][/td][/tr][/table]\n\n"
+    },
+    {
+        type: "zinfo",
+        body_start: '[table class="zmod_box zinfo"][tr][td style="padding-right: 10px;" width="60px;"][icon="fa fa-info"][/icon][div]',
+        body_end: "[/div][/td][/tr][/table]\n\n"
+    }
+];
+
+var zModMessages = [{
+        name: "Alert example",
+        message: '[b]Alert[/b] \nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        group_id: 0,
+        type: "zalert"
+    },
+    {
+        name: "Success example",
+        message: '[b]Success[/b] \nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        group_id: 0,
+        type: "zsuccess"
+    },
+    {
+        name: "Default example",
+        message: '[b]Default[/b] \nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        group_id: 1,
+        type: "zdefault"
+    },
+    {
+        name: "Warning example",
+        message: '[b]Warning[/b] \nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        group_id: 1,
+        type: "zwarning"
+    },
+    {
+        name: "Info example",
+        message: '[b]Info[/b] \nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        group_id: 1,
+        type: "zinfo"
+    }
+];
+
+var zModGroups = [{
+        id: 0,
+        name: 'Administrators'
+    },
+    {
+        id: 1,
+        name: 'Moderators'
+    }
+];
+
+$.getScript( zModConfig[0].source, function(textStatus) {
+    console.log( "zModStatus:" + textStatus);
+});
